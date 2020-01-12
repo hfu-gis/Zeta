66f3b83ff943e5bfc528252a609e320009d33ceb
MelliFl <57178331+MelliFl@users.noreply.github.com>
2020-01-12 14:31:42 +0100

    Add files via upload
    Finale Login Seite

--- Login.vue
+++ Login.vue
@@ -2,7 +2,7 @@
     <v-app id="inspire">
         <v-content>
             <v-container
-                    class="fill-height"
+
                     fluid
             >
                 <v-row
@@ -14,65 +14,38 @@
                             sm="8"
                             md="4"
                     >
-
-        <v-card class="elevation-12">
+                        <v-card class="box">
                             <v-toolbar
-                                    color="primary"
+                                    color="red"
                                     dark
                                     flat
                             >
-                                <v-toolbar-title>Login form</v-toolbar-title>
+                                <v-toolbar-title>Login</v-toolbar-title>
                                 <v-spacer />
-                                <v-tooltip bottom>
-                                    <template v-slot:activator="{ on }">
-                                        <v-btn
-                                                :href="source"
-                                                icon
-                                                large
-                                                target="_blank"
-                                                v-on="on"
-                                        >
-                                            <v-icon>mdi-code-tags</v-icon>
-                                        </v-btn>
-                                    </template>
-                                    <span>Source</span>
-                                </v-tooltip>
-                                <v-tooltip right>
-                                    <template v-slot:activator="{ on }">
-                                        <v-btn
-                                                icon
-                                                large
-                                                href="https://codepen.io/johnjleider/pen/pMvGQO"
-                                                target="_blank"
-                                                v-on="on"
-                                        >
-                                            <v-icon>mdi-codepen</v-icon>
-                                        </v-btn>
-                                    </template>
-                                    <span>Codepen</span>
-                                </v-tooltip>
                             </v-toolbar>
                             <v-card-text>
                                 <v-form>
                                     <v-text-field
-                                            label="Login"
-                                            name="login"
-                                            prepend-icon="person"
+                                            class="haja"
+                                            label="Name"
+                                            name="name"
                                             type="text"
+                                            v-model="username"
                                     />
 
                                     <v-text-field
+                                            class="hanoi"
                                             id="password"
-                                            label="Password"
+                                            label="Passwort"
                                             name="password"
-                                            prepend-icon="lock"
                                             type="password"
+                                            v-model="password"
                                     />
                                 </v-form>
                             </v-card-text>
                             <v-card-actions>
                                 <v-spacer />
-                                <v-btn color="primary">Login</v-btn>
+                                <v-btn color="red">Login</v-btn>
                             </v-card-actions>
                         </v-card>
                     </v-col>
@@ -84,8 +57,18 @@
 
 <script>
     export default {
+        name:"Login",
         props: {
             source: String,
         },
-    }
+        date(){
+            return {
+                password:' ',
+                username:' '
+            }
+        },
+    };
 </script>
+<style scoped>
+.box{width: 1000px}
+</style>}
